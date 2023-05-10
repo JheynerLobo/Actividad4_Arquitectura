51afe2b2c03c2e1ea7bef795adc93d7494d527d5
JheynerLobo <84026784+JheynerLobo@users.noreply.github.com>
2023-05-10 17:27:45 -0500

    Update Networking.kt

--- app/src/main/java/com/decimalab/easytask/model/remote/Networking.kt
+++ app/src/main/java/com/decimalab/easytask/model/remote/Networking.kt
@@ -3,10 +3,7 @@ package com.decimalab.easytask.model.remote
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-/**
- * Created by Shakil Ahmed Shaj on 14,April,2020.
- * shakilahmedshaj@gmail.com
- */
+
 object Networking {
     fun create(baseUrl: String): NetworkService {
         return Retrofit.Builder()
