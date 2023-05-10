c3a922f312197059f51cb995479d6d59cdba6805
JheynerLobo <84026784+JheynerLobo@users.noreply.github.com>
2023-05-10 17:33:27 -0500

    Update TaskCallBack.kt

--- app/src/main/java/com/decimalab/easytask/view/adaptor/TaskCallBack.kt
+++ app/src/main/java/com/decimalab/easytask/view/adaptor/TaskCallBack.kt
@@ -2,10 +2,6 @@ package com.decimalab.easytask.view.adaptor
 
 import android.view.View
 
-/**
- * Created by Shakil Ahmed Shaj on 17,April,2020.
- * shakilahmedshaj@gmail.com
- */
 interface TaskCallBack {
 
     fun onTaskClick(view: View, position: Int, isLongClick: Boolean)
