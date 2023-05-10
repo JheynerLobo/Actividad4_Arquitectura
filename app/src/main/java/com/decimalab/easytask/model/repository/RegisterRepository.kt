b84bfe1275056bfa3b0b287c97268ab2c682bd11
JheynerLobo <84026784+JheynerLobo@users.noreply.github.com>
2023-05-10 17:30:49 -0500

    Update RegisterRepository.kt

--- app/src/main/java/com/decimalab/easytask/model/repository/RegisterRepository.kt
+++ app/src/main/java/com/decimalab/easytask/model/repository/RegisterRepository.kt
@@ -3,10 +3,6 @@ package com.decimalab.easytask.model.repository
 import com.decimalab.easytask.model.remote.NetworkService
 import com.decimalab.easytask.model.remote.request.auth.RegisterRequest
 
-/**
- * Created by Shakil Ahmed Shaj on 14,April,2020.
- * shakilahmedshaj@gmail.com
- */
 class RegisterRepository(private val networkService: NetworkService) {
 
     suspend fun register(registerRequest: RegisterRequest) =
