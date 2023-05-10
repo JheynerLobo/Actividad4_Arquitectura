d5459f4825bc912e15b0e247b8213824e627d53b
JheynerLobo <84026784+JheynerLobo@users.noreply.github.com>
2023-05-10 17:28:37 -0500

    Update DeleteTaskRepository.kt

--- app/src/main/java/com/decimalab/easytask/model/repository/DeleteTaskRepository.kt
+++ app/src/main/java/com/decimalab/easytask/model/repository/DeleteTaskRepository.kt
@@ -5,10 +5,7 @@ import com.decimalab.easytask.model.local.entity.TaskEntity
 import com.decimalab.easytask.model.remote.NetworkService
 import com.decimalab.easytask.model.remote.request.task.DeleteTaskRequest
 
-/**
- * Created by Shakil Ahmed Shaj on 30,April,2020.
- * shakilahmedshaj@gmail.com
- */
+
 class DeleteTaskRepository(
     private val networkService: NetworkService,
     private val appDatabase: AppDatabase
