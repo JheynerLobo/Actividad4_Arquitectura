49b83dd69b1fda2cc23d5db782582602a345600e
JheynerLobo <84026784+JheynerLobo@users.noreply.github.com>
2023-05-10 17:32:30 -0500

    Update AppThemeMode.kt

--- app/src/main/java/com/decimalab/easytask/util/AppThemeMode.kt
+++ app/src/main/java/com/decimalab/easytask/util/AppThemeMode.kt
@@ -1,9 +1,6 @@
 package com.decimalab.easytask.util
 
-/**
- * Created by Shakil Ahmed Shaj on 14,May,2020.
- * shakilahmedshaj@gmail.com
- */
+
 enum class AppThemeMode {
 
     LIGHT, DARK, SYSTEM, BATTERY
