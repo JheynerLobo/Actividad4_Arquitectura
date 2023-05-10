3eba99ff8e36c0b2bc2889eff5901224185fad35
JheynerLobo <84026784+JheynerLobo@users.noreply.github.com>
2023-05-10 17:25:45 -0500

    Update RegisterRequest.kt

--- app/src/main/java/com/decimalab/easytask/model/remote/request/auth/RegisterRequest.kt
+++ app/src/main/java/com/decimalab/easytask/model/remote/request/auth/RegisterRequest.kt
@@ -2,10 +2,7 @@ package com.decimalab.easytask.model.remote.request.auth
 
 import com.google.gson.annotations.SerializedName
 
-/**
- * Created by Shakil Ahmed Shaj on 14,April,2020.
- * shakilahmedshaj@gmail.com
- */
+
 data class RegisterRequest(
     @SerializedName("name")
     val name: String,
