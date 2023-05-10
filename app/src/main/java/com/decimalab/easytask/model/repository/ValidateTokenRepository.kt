ba17a20f7233849476e1c97b1ebd980f28ef45f9
JheynerLobo <84026784+JheynerLobo@users.noreply.github.com>
2023-05-10 17:31:43 -0500

    Update ValidateTokenRepository.kt

--- app/src/main/java/com/decimalab/easytask/model/repository/ValidateTokenRepository.kt
+++ app/src/main/java/com/decimalab/easytask/model/repository/ValidateTokenRepository.kt
@@ -2,10 +2,6 @@ package com.decimalab.easytask.model.repository
 
 import com.decimalab.easytask.model.remote.NetworkService
 
-/**
- * Created by Shakil Ahmed Shaj on 15,April,2020.
- * shakilahmedshaj@gmail.com
- */
 class ValidateTokenRepository(private val networkService: NetworkService) {
 
     suspend fun validateToken(token: String) = networkService.validateToken(token)
